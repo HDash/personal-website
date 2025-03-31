75c980366716363bd83bc5b38bfc53c8124413ad
HDash <16350928+HDash@users.noreply.github.com>
2025-03-31 14:48:49 +0100

    Enable static export

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -21,13 +21,14 @@ export default function Skills() {
           >
             <li className="opacity-90 cursor-pointer hover:opacity-50">
               <span>{skill}</span>
+              <span className="text-xs opacity-30"> →</span>
               {subSkills.length > 0 && (
                 <span className="ml-1 text-xs text-gray-400"></span>
               )}
             </li>
             
             {hoveredSkill === skill && subSkills.length > 0 && (
-              <div className="absolute ml-2 left-30 sm:left-70 top-0 shadow-md rounded-xl p-3 min-w-48 z-10 border dark:bg-gray-950 bg-white dark:border-gray-100 border-gray-500">
+              <div className="absolute ml-2 left-30 sm:left-70 top-0 shadow-md rounded-xl p-3 min-w-48 z-10 border dark:bg-gray-950 bg-teal-50 dark:border-gray-300 border-gray-500">
                 <p className="font-medium mb-1">{skill}</p>
                 {subSkills.length > 0 ? (
                   <ul className="list-disc list-inside">
